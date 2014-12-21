216554111ec54fdedb2439ff9181ed2dbae38de0
Francis Colas <francis.colas@inria.fr>
2014-12-21 14:39:13 +0100

    filtering svn subrepos

--- repo_manager.py
+++ repo_manager.py
@@ -4,11 +4,17 @@ import os
 
 def list_repo(directory):
     "Find recursively git and svn repositories."
+    git_repos = []
+    svn_repos = []
     for dirpath, dirnames, filenames in os.walk(directory):
         if '.git' in dirnames:
-            print('git repository:', dirpath)
+            git_repos.append(dirpath)
         elif '.svn' in dirnames:
-            print('svn repository:', dirpath)
+            if any(dirpath.startswith(svn_repo) for svn_repo in svn_repos):
+                # don't allow svn repos inside others (assumed to be externals)
+                continue
+            svn_repos.append(dirpath)
+    return git_repos, svn_repos
 
 
 def main():
@@ -18,7 +24,11 @@ def main():
                         help='the directory to start searching in.')
     args = parser.parse_args()
     # invoking main function
-    list_repo(args.directory)
+    git_repos, svn_repos = list_repo(args.directory)
+    for r in git_repos:
+        print('git', r)
+    for r in svn_repos:
+        print('svn', r)
 
 
 if __name__ == '__main__':
