082bd8250752769252edcb8b38354e3615d558b0
Francis Colas <francis.colas@inria.fr>
2014-12-22 18:13:19 +0100

    get config from git repositories

--- repo_manager.py
+++ repo_manager.py
@@ -1,14 +1,39 @@
 import argparse
 import os
+from subprocess import check_output, CalledProcessError
+
+
+def get_config(git_dir):
+    """Find useful config of a git directory."""
+    try:
+        cwd = os.getcwd()
+        os.chdir(git_dir)
+        out = check_output(['git', 'config', '--local', '--list'],
+                           universal_newlines=True)
+        config = dict(line.split('=') for line in out.strip().split())
+        for k in list(config.keys()):
+            if k.startswith('core.'):
+                config.pop(k)
+        return config
+    except CalledProcessError:
+        raise ValueError("%s does not seem to be a proper git repository." %
+                         git_dir)
+    finally:
+        # executed before return
+        os.chdir(cwd)
 
 
 def list_repo(directory):
-    "Find recursively git and svn repositories."
+    """Find recursively git and svn repositories."""
     git_repos = []
     svn_repos = []
     for dirpath, dirnames, filenames in os.walk(directory):
         if '.git' in dirnames:
-            git_repos.append(dirpath)
+            try:
+                config = get_config(dirpath)
+                git_repos.append((dirpath, config))
+            except ValueError:
+                pass
         elif '.svn' in dirnames:
             if any(dirpath.startswith(svn_repo) for svn_repo in svn_repos):
                 # don't allow svn repos inside others (assumed to be externals)
